69313d7f5b2647a46857a8c3a44981c8df642c46
hummingjay <jays7mill@gmail.com>
2022-12-14 05:45:02 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints 10 times
+ * the alphabet in lowercase followed by new line
+ *
+ * Return: 0 success
+ */
+void print_alphabet_x10(void)
+{
+	int n, tot;
+
+	tot = 0;
+
+	while (tot < 10)
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		tot++;
+		_putchar('\n');
+}
