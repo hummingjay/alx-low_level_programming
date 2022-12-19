4ba590dbe10d6ff17024fe597579e81ee0b781da
hummingjay <jays7mill@gmail.com>
2022-12-19 18:20:55 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of string
+ * @s: string to count
+ *
+ * Return: string length value
+ */
+int _strlen(char *s)
+{
+	for(int i = 0; *s = '\0'; s++)
+	{
+		i++;
+	}
+	return (i);
+}
