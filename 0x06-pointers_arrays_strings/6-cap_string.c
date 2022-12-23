cc3f4263fb64e4c20345b7686f53c2f6c8660012
hummingjay <jays7mill@gmail.com>
2022-12-23 21:54:45 +0300

    5-string_toupper.c - change to lowercase from upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes words of a string
+ * @str: pointer to string
+ *
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'b')
+		{
+			str[i] += 32;
+		}
+	}
+	return (str);
+}
