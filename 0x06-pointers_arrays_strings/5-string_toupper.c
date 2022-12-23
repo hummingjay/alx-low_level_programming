5fe7347f2cc0bad31bc3edcd6999ffd692bc5c2e
hummingjay <jays7mill@gmail.com>
2022-12-23 21:32:47 +0300

    5-string_toupper.c - change to lowercase from upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercse letters
+ * of a string to uppercase
+ *
+ * @str: pointer to string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= "a" && str[i] <= "z")
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
