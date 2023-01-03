62de49269d412782d0f4d8facec14705e6201110
hummingjay <jays7mill@gmail.com>
2023-01-04 01:11:50 +0300

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, value;
-	
-	value=0
+	unsigned int i, n, value, check;
+
+	value = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (accept[i] == s[i])
+		check = 0;
+
+		for (n = 0; accept[n] != '\0'; n++)
 		{
-			value++;
+			if (accept[n] == s[i])
+			{
+				value++;
+				check = 1;
+			}
 		}
+
+		if (check == 0)
+			return (value);
 	}
+
 	return (value);
 }
