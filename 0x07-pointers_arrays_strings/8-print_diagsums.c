936b63fc3ce7eec905895f04ccc099cc9a2f7800
hummingjay <jays7mill@gmail.com>
2023-01-04 20:03:58 +0300

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,20 @@
 
 void print_diagsums(int *a, int size)
 {
-  int sum1 = 0;  /* Sum of primary diagonal*/
-  int sum2 = 0;  /* Sum of secondary diagonal*/
+	int sum1 = 0;  /* Sum of primary diagonal*/
+	int sum2 = 0;  /* Sum of secondary diagonal*/
 
-  /* Iterate through the matrix and calculate the sums of the diagonals*/
-  for (int i = 0; i < size; i++)
-  {
-    sum1 += *(a + i * size + i);
-    sum2 += *(a + i * size + (size - i - 1));
-  }
+	/* Iterate through the matrix and calculate the sums of the diagonals*/
+	for (int i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
+	}
 
-  /* Print the results*/
-  printf("Sum of primary diagonal: %d\n", sum1);
-  printf("Sum of secondary diagonal: %d\n", sum2);
+	/* Print the results*/
+
+	printf("Sum of primary diagonal: %d\n", sum1);
+	printf("Sum of secondary diagonal: %d\n", sum2);
+
+	return(NULL);
 }
