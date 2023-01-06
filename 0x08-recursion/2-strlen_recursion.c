7cb54f5775d0fa55a96ffd5ad49c557adef4ce54
hummingjay <jays7mill@gmail.com>
2023-01-06 22:29:55 +0300

    2-strlen_recursion.c - corrected

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -18,7 +18,8 @@ int _strlen_recursion(char *s)
 	}
 	else
 	{
-		/**return value 1 which will be added up for each
+		/**
+		 * return value 1 which will be added up for each
 		 * string value to find length so 1 + 1 and
 		 * move the value up by 1 upto where
 		 * code will end with the + 0,
