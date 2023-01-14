ab8e32c7df5abd15b8d5ba9669eb14db626780e6
hummingjay <jays7mill@gmail.com>
2023-01-14 23:31:40 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+#ifdef MAIN_H
 #define MAIN_H
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
@@ -6,77 +6,6 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 #endif
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-main.h[+] [unix] (02:59 01/01/1970)                                                                                            4,1 All
--- INSERT --
-~
 ~
 ~
 ~
