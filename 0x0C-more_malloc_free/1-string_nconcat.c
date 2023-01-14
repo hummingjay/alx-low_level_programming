ec96d4b3f44bc7ce142c1dbbe05457396d55c85d
hummingjay <jays7mill@gmail.com>
2023-01-14 23:25:54 +0300

    1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - function that concanates two strings
+ * @s1: string one destination
+ * @s2: string to truncate to n bytes
+ * @n: size of string size to truncate to
+ *
+ * Return: Null if fail if n >= s2 use s2 complete
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *copy;
+	unsigned int s1size, s2size;
+
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
+	s1size = sizeof(s1);
+	s2size = sizeof(s2);
+
+	buffersize = s1size + s2size;
+
+	copy = malloc(buffersize * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
+
+	for(n = 0; n <= buffersize - 1; n++)
+	{
+		if (n < s1size)
+		{
+			copy[n] = s1[n];
+		}
+		else
+		{
+			copy[n] = s2[i - s1size];
+		}
+	}
+	s1[buffersize] = '\0';
+
+	return (copy);
+}
