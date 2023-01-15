58487bfc94868f665bff65c1dfa9a4da3b026249
hummingjay <jays7mill@gmail.com>
2023-01-15 04:14:35 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	point = malloc(size * nmemeb);
+	point = malloc(size * nmemb);
 
 	if (point == NULL)
 		return (NULL);
