6e399b6de84ab878378b545a052a6c799682bf55
hummingjay <jays7mill@gmail.com>
2023-01-17 14:02:20 -0800

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
 #ifndef ABS_H
 #define ABS_H
 /*?: - means if x (something) else (something else)*/
-#define ABS(X) (((X) < 0) ? ((X) * -1) : ((X)*1))
+#define ABS(X) (((X) < 0) ? ((X) * -1) : ((X) * 1))
 #endif
