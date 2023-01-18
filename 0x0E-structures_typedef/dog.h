3e9fde4036006a566ae8f7c7094a4e20164abcb5
hummingjay <jays7mill@gmail.com>
2023-01-17 17:11:57 -0800

    added documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct - description of dog
+ * @name - name of dog
+ * @age - age of dog
+ * @owner - owner of dog
+ *
+ * Description: about the dog and data about it
+ */
 struct dog
 {
 	char *name;
