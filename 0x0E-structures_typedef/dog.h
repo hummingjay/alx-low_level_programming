084250a2dd7dbf8c337fd8df76b21c82a9c10ccf
hummingjay <jays7mill@gmail.com>
2023-01-18 05:34:00 +0300

    dog.h v2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,7 @@ typedef struct dog
 }dog_t;
 /*more prototypes for functions*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 void print_dog(struct dog *d);
 #endif
