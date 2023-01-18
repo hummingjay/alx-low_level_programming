be93afae60e5890b2068550ba74ecf2ec4c0fd97
hummingjay <jays7mill@gmail.com>
2023-01-17 18:07:31 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	{
 		/* if else if using the ternery operator ?:*/
 		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
-		printf("Age: %f\n", d->age)
+		printf("Age: %f\n", d->age);
 		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
 	}
 }
