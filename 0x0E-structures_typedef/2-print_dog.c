25082963b40f767b4becfd617074f2d447015131
hummingjay <jays7mill@gmail.com>
2023-01-17 18:02:46 -0800

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints the defined attributes of the dog
+ * @d: pointer to dog struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		/* if else if using the ternery operator ?:*/
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
+		d->age ? (printf("Age: %f\n", d->age)) : (printf("Age: (nil)\n"));
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil)\n"));
+	}
+}
