3b877237b3d53373fb09fe1047847fa9321dd266
hummingjay <jays7mill@gmail.com>
2023-01-25 23:02:21 +0300

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -5,12 +5,13 @@ int main()
     FILE *fp;
     char buffer[1000];
     size_t bytes_read;
+    int i;
 
     fp = fopen("100-main_opcodes", "rb");
     bytes_read = fread(buffer, 1, sizeof(buffer), fp);
     fclose(fp);
 
-    for (int i = 0; i < bytes_read; i++) {
+    for (i = 0; i < bytes_read; i++) {
         printf("%02x ", buffer[i]);
     }
     printf("\n");
