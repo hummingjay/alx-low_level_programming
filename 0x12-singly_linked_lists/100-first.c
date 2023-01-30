2406fd71b21a9212eae93770f9d6c677f98c8fc1
hummingjay <jays7mill@gmail.com>
2023-01-30 12:52:18 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * pre_main: function to print string before main is
+ * executed.
+ *
+ * Return: 0
+ */
+void pre_main(void) __attribute__ ((constructor));
+
+void pre_main(void)
+{
+	char *message;
+
+	message = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	printf("%s", messsage);
+}
