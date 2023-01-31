36dc2a57b6b24096965eef4b28c422a94f2dd9a7
hummingjay <jays7mill@gmail.com>
2023-01-31 10:57:45 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * *add_node - function that inserts new node at the
@@ -13,16 +14,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	int i;
+	list_t *new;
 
 	if (head == NULL)
 		return (NULL);
 
-	/*the new node type cast then allocate space to it*/
-	new = (struct list_t *) malloc(sizeof(list_t));
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	for (i = 0; str[i] != '\0'; i++);
 	new->len = i;
 	new->str = strdup(str);
 	new->next = *head;
