55ee59bfcb73dc4dcf32989c09d311af85d4565e
hummingjay <jays7mill@gmail.com>
2023-02-03 00:23:27 +0300

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,23 +15,27 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 /*assign ptr to head, then traverse list with ptr*/
 	ptr = *head;
 
-/*create new node that consists the data part and next part*/
+	/**create new node that consists
+	 * the data part and next part*/
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
 		return (NULL);
 
-/*updating the data(beginning of node)and next part of new node*/
+	/**updating the data(beginning of node)
+	 * and next part of new node*/
 	new->n = n;
 	new->next = NULL;
 
-/*to traverse the list now if ptr is not final node*/
+	/**to traverse the list now 
+	 * if ptr is not the final node*/
 	while (ptr->next != NULL)
 	{
 		/*updates the ptr sequentially*/
 		ptr = ptr->next;
 	}
-/*when the above loop is true it exits then update value below*/
+	/**when the above loop is true 
+	 * it exits then update value below*/
 	ptr->next = new;
 	return (new);
 }
