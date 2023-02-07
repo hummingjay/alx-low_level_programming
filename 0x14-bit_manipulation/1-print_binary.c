85e9c2f6f4b78fc543912a5767d42474ff64bfac
hummingjay <jays7mill@gmail.com>
2023-02-07 23:22:01 +0300

    1-print_binary.c overhaul

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,23 +9,15 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0, k, bit;
+	int i, flag;
 
 	if (n == 0)
 		_putchar('0');
-
-	while (n != 0)
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		i++;
-		n = n >> 1;
-	}
-	for (bit = i - 1; bit >= 0; bit--)
-	{
-		k = n >> bit;
-
-		if (k & 1)
-			_putchar('1');
-		else
-			_putchar('0');
+		if ((n >> i) & 1)
+		flag = 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
