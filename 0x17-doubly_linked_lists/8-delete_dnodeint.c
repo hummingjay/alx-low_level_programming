253d800ca442c9306de300f29c0c3729c6f36bb3
hummingjay <jays7mill@gmail.com>
2023-03-16 16:53:09 -0700

    no white

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL) /* empty list */
 		return (-1);
 
-        if (index == 0) /* delete head */
+	if (index == 0) /* delete head */
 	{
 		*head = (*head)->next;
 		if (*head != NULL)
@@ -27,16 +27,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	/* find the node to delete */
 	for (i = 0; i < index - 1 && temp != NULL; i++)
-	    temp = temp->next;
+		temp = temp->next;
 
+	/* index out of range */
 	if (temp == NULL || temp->next == NULL)
-	       	/* index out of range */
 		return (-1);
 
 	node_to_delete = temp->next;
 	temp->next = node_to_delete->next;
 	if (node_to_delete->next != NULL)
-	    node_to_delete->next->prev = temp;
+		node_to_delete->next->prev = temp;
 	free(node_to_delete);
 	return (1);
 }
