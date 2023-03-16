136547fa9c0bc00e708ac7e7965771598ca082d9
hummingjay <jays7mill@gmail.com>
2023-03-16 12:27:25 -0700

    removed n

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	new_node = malloc(sizeof(dlistint_n));
+	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 	{
 		return (NULL);
