ec17155b706a9ea29d4e8f43be6ee752799ec85d
hummingjay <jays7mill@gmail.com>
2023-03-26 18:47:48 +0300

    executable

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/hummingjay/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mynum.so 
+wget -p /tmp https://github.com/hummingjay/alx-low_level_programming/blob/master/0x18-dynamic_libraries/mynum.so
 export LD_PRELOAD=/tmp/mynum.so
